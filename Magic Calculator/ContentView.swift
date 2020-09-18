6b85bb02b4ab36f20bfa3c9d4ba1d3c88ea22c5b
Salim <hi@salimhafid.com>
2020-09-18 16:07:14 -0700

    Initial Commit

---
+++ Magic Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Magic Calculator
+//
+//  Created by Salim Hafid on 9/18/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Magic Calculator/Magic_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Magic_CalculatorApp.swift
+//  Magic Calculator
+//
+//  Created by Salim Hafid on 9/18/20.
+//
+
+import SwiftUI
+
+@main
+struct Magic_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
